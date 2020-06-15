3f44a92692ac0d0412ebe198075c71232c5926dc
kodaTheCoder <marvincollins14@gmail.com>
2020-06-16 01:06:56 +0300

    added templating folder

--- course-website/main.go
+++ course-website/main.go
@@ -17,9 +17,22 @@ func contact(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprint(writer, "<h1>This is a contact page</h1> <br /> Email us: marvin@appslab.co.ke")
 }
 
+func notFound(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/html")
+	writer.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(writer, "The page you are looking is not found")
+}
+
+func faq(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(writer, "This is a FAQ page for our good users <br /> <ul><li>What we do during our spare time</li><li>Most of the time we read novels and tell each other stories about star trek</li></ul>")
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", home)
 	router.HandleFunc("/contact", contact)
+	router.HandleFunc("/faq", faq)
+	router.NotFoundHandler = http.HandlerFunc(notFound)
 	http.ListenAndServe(":8000", router)
 }
